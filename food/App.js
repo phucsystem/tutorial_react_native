6db261d9e31e60acf2c8594dd037bef24b048cc9
Phuc Dang <phucsystem@gmail.com>
2020-05-05 06:17:03 +0700

    Implement food

--- food/App.js
+++ food/App.js
@@ -1,18 +1,19 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
 import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const navigator = createStackNavigator(
-  {
-    Search: SearchScreen,
-    ResultsShow: ResultsShowScreen
-  },
-  {
-    initialRouteName: 'Search',
-    defaultNavigationOptions: {
-      title: 'Business Search'
+    {
+        Search: SearchScreen,
+        ResultsShow: ResultsShowScreen
+    },
+        {
+            initialRouteName: 'Search',
+            defaultNavigationOptions: {
+                title: 'Business Search'
+            }
     }
-  }
 );
 
 export default createAppContainer(navigator);
